4896cb0dcaddece0f9ea30fe400735cf194408ce
rachelishmuel <racheli.g@elevation.ac>
2019-10-15 05:24:37 -0700

    first commit

---
+++ Animal.cpp
@@ -0,0 +1,16 @@
+//
+// Created by rachelishmuel on 9/23/19.
+//
+
+#include "Animal.h"
+
+Animal::Animal(const std::string &name, unsigned int life_expectanc):m_name(name),m_life_expectanc(life_expectanc){}
+
+
+
+std::ostream& operator<<(std::ostream& os, const Animal*obj)
+{
+    os << obj->getDescription();// write obj to stream
+    return os;
+}
+
